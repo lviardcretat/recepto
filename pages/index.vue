44ecd80aff97a9a23859bbc2f216f80d449032b0
marquito73 <theobegyn@live.com>
2024-12-08 21:53:45 +0100

    feat: Adding searchbar on index view (not working)

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+	<UDashboardPage class="homepage h-full w-full">
+		<SearchBar
+		/>
+	</UDashboardPage>
+</template>
+
+<style lang="scss">
+	.homepage {
+		display: flex;
+		flex-direction: column;
+		padding: 2rem;
+
+		justify-content: center;
+		align-items: center;
+
+		> .searchBar {
+			width: 75%;
+		}
+	}
+</style>
