34244681878a3ac8aa13e384fbaa9a5b8e4d5aeb
lviardcretat <loic.viard-cretat@proton.me>
2024-12-09 00:24:52 +0100

    improvement: add code structure for proposals for research

--- components/SearchBar.vue
+++ components/SearchBar.vue
@@ -7,20 +7,28 @@
 		placeholder="Search a recipe ..."
 		size="xl"
 		v-model="searchValue"
-		@update:model-value="test"
 	/>
+	<p>{{ searchValue }}</p>
+	<p>{{ names }}</p>
   </div>
 </template>
 
 <script lang="ts" setup>
-	import { ref } from 'vue';
+import { ref, watch } from 'vue';
 
-	const searchValue = ref<string>('');
+let searchValue = ref<string>('');
+let names = ref();
 
-	const test = (newSearch: string) => {
-		console.log(newSearch);
-		alert("TEST");
-	};
+const { data } = await useFetch('/api/recipesCategories/test', {
+	query: {
+		name: searchValue,
+	},
+	/* Changing the searchValue will trigger a refetch */
+	watch: [searchValue],
+	onResponse({ request, response, options }) {
+		names = response._data;
+	},
+});
 </script>
 
 <style lang="scss">
