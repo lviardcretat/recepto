0b3b910f9fcb2b645cd5176c7f245c88e78bd682
lviardcretat <loic.viard-cretat@proton.me>
2025-01-09 11:01:11 +0100

    ci: switching to pgsql

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "A prisma recepto project";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  outputs = { nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+    	pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      	devShell = pkgs.mkShell {
+			nativeBuildInputs = [ pkgs.bashInteractive ];
+			buildInputs = with pkgs; [
+				prisma
+				biome
+				lefthook
+				nodejs
+				openssl
+				pnpm
+				prisma-engines
+			];
+			shellHook = with pkgs; ''
+				export PRISMA_MIGRATION_ENGINE_BINARY="${prisma-engines}/bin/migration-engine"
+				export PRISMA_QUERY_ENGINE_BINARY="${prisma-engines}/bin/query-engine"
+				export PRISMA_QUERY_ENGINE_LIBRARY="${prisma-engines}/lib/libquery_engine.node"
+				export PRISMA_INTROSPECTION_ENGINE_BINARY="${prisma-engines}/bin/introspection-engine"
+				export PRISMA_FMT_BINARY="${prisma-engines}/bin/prisma-fmt"
+			'';
+      	};
+    });
+}
