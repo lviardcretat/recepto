36d440590e0f0993dae29b9cc1c1568d5053f21a
loic <loic.viard-cretat@proton.me>
2025-02-08 22:51:41 +0100

    ci: fixed prisma compatibility with vite and nitro

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,64 +1,73 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	compatibilityDate: '2024-04-03',
-	ssr: true,
-	devtools: { enabled: true },
-	build: {
-		transpile: ['@prisma/client'],
-	},
-	extends: ['@nuxt/ui-pro'],
-	modules: [
-		'@nuxt/image',
-		'@nuxt/ui',
-		'@nuxtjs/i18n',
-		'@pinia/nuxt',
-		'@prisma/nuxt',
-		'nuxt-swiper',
-		'@nuxthub/core',
-		'nuxt-zod-i18n',
-	],
-	css: ['~/assets/css/main.css'],
-	postcss: {
-		plugins: {
-			tailwindcss: {},
-			autoprefixer: {},
-		},
-	},
-	vue: {
-		compilerOptions: {
-			isCustomElement: (tag) => tag.includes('swiper-'),
-		},
-	},
-	app: {
-		head: {
-			htmlAttrs: {
-				lang: 'fr',
-			},
-			title: 'Recepto',
-			charset: 'utf-8',
-			meta: [],
-			link: [],
-		},
-	},
-	prisma: {
-		installStudio: true,
-		autoSetupPrisma: true,
-	},
-	i18n: {
-		langDir: 'locales',
-		defaultLocale: 'fr',
-		locales: [
-			{ code: 'fr', file: 'fr.ts' },
-			{ code: 'en', file: 'en.ts' },
-		],
-		lazy: true,
-		strategy: 'no_prefix',
-		vueI18n: './i18n/i18n.config.ts',
-	},
-	zodI18n: {
-		localeCodesMapping: {
-			'en-GB': 'en',
-			'fr-FR': 'fr',
-		},
-	},
+				compatibilityDate: '2024-04-03',
+				ssr: true,
+				devtools: { enabled: true },
+				extends: ['@nuxt/ui-pro'],
+				nitro: {
+					experimental: {
+					  wasm: true
+					}
+				  },
+				vite: {
+					resolve: {
+					  alias: {
+						".prisma/client/index-browser": "./node_modules/.prisma/client/index-browser.js"
+					  }
+					}
+				  },
+				modules: [
+								'@nuxt/image',
+								'@nuxt/ui',
+								'@nuxtjs/i18n',
+								'@pinia/nuxt',
+								'@prisma/nuxt',
+								'nuxt-swiper',
+								'@nuxthub/core',
+								'nuxt-zod-i18n',
+				],
+				css: ['~/assets/css/main.css'],
+				postcss: {
+								plugins: {
+												tailwindcss: {},
+												autoprefixer: {},
+								},
+				},
+				vue: {
+								compilerOptions: {
+												isCustomElement: (tag) => tag.includes('swiper-'),
+								},
+				},
+				app: {
+								head: {
+												htmlAttrs: {
+																lang: 'fr',
+												},
+												title: 'Recepto',
+												charset: 'utf-8',
+												meta: [],
+												link: [],
+								},
+				},
+				prisma: {
+								installStudio: true,
+								autoSetupPrisma: true,
+				},
+				i18n: {
+								langDir: 'locales',
+								defaultLocale: 'fr',
+								locales: [
+												{ code: 'fr', file: 'fr.ts' },
+												{ code: 'en', file: 'en.ts' },
+								],
+								lazy: true,
+								strategy: 'no_prefix',
+								vueI18n: './i18n/i18n.config.ts',
+				},
+				zodI18n: {
+								localeCodesMapping: {
+												'en-GB': 'en',
+												'fr-FR': 'fr',
+								},
+				},
 });
