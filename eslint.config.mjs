eb016862824ff4b3ec170c15a23aadca05f4471a
lviardcretat <loic.viard-cretat@proton.me>
2025-12-06 20:22:09 +0100

    [ci] added vue-mess-detector lib

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,5 +4,6 @@ export default withNuxt().override('nuxt/typescript/rules', {
   rules: {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-empty-object-type': 'off',
+    '@typescript-eslint/no-invalid-void-type': 'off',
   },
 });
